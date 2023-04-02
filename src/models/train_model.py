cdd949612d79eb7f9b85d6a4a9b51c2f6b673220
jmannisto <67613785+jmannisto@users.noreply.github.com>
2023-04-02 16:36:02 +0300

    added training methods and updated devices

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -12,6 +12,7 @@ import time
 import copy
 import sys
 import os
+from sklearn.metrics import classification_report
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__)) #having issues with file path in VSCode
 sys.path.append(os.path.dirname(SCRIPT_DIR))
 sys.path.append('../data')
@@ -29,43 +30,71 @@ NUM_CLASSES = 14
 DATA_DIR = '../../data/interim/'
 TRAIN_TEST_DEV_RATIO = [0.8, 0.1, 0.1] #TODO: pass this through to data load
 
+#--- set up device ---
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
 # --- set up ---
 alexnet = models.alexnet(weights = 'DEFAULT')
 optimizer = optim.Adam(alexnet.parameters(), lr=LR, weight_decay=1e-4)
 criterion = nn.BCEWithLogitsLoss()
 
 # --- editing parameters of pretrained model ---
-#taken from pytorch
-#set up for AlexNet
-def initialize_model(num_classes, feature_extract, use_pretrained=True):
-    # Initialize these variables which will be set in this if statement. Each of these
-    #   variables is model specific.
-    model_ft = models.alexnet(pretrained=use_pretrained) #pretrained = use final layer
-    set_parameter_requires_grad(model_ft, feature_extract)
-    num_ftrs = model_ft.classifier[6].in_features
-    model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes) #change outputs to 14 
-    input_size = 224 #image size needed
+#taken from pytorch: https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html 
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+    """
+    Initialize model based on model name passed, each variable is model specific
+    """
+    if model_name == "alexnet":
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224 
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+    else:
+        print("Invalid model name, exiting...")
+        exit()
     return model_ft, input_size
 
 #necessary if we only want to update the final layer
+#also from pytorch: https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html 
 def set_parameter_requires_grad(model, feature_extracting):
     if feature_extracting:
         for param in model.parameters():
             param.requires_grad = False
 
-model_ft, input_size = initialize_model(NUM_CLASSES, feature_extract=True, use_pretrained=True)
-
-params_to_update = model_ft.parameters()
-print("Params to learn:")
-params_to_update = []
-for name,param in model_ft.named_parameters():
-    if param.requires_grad == True:
-        params_to_update.append(param)
-        #print("\t",name)
-optimizer_ft = optim.SGD(params_to_update, lr=0.001, momentum=0.9)
 
 # --- Dataset initialization ---
 transformations = transforms.Compose([
+        #transforms.ColorJitter()
+        #transforms.RandomRotation(25) #added occasionally for testing
         transforms.ToPILImage(),
         transforms.Resize(input_size),
         transforms.ToTensor(),
@@ -73,33 +102,55 @@ transformations = transforms.Compose([
     ])
 
 print('Preparing dataset')
-#train_loader, dev_loader, test_loader = data_load.get_dataset(BATCH_SIZE_TRAIN)
 train_loader, dev_loader, test_loader = data_load.get_dataset(BATCH_SIZE_TRAIN, transformations)
 
 # --- eval functions ---
-#TODO: edit accuracy method, currrently incorrect
 def accuracy(output, gold):
-    output = torch.sigmoid(output) #no sigmoid at the end of the model so implemented here to round to 1 or 0 next
+    total = 0
+    correct = 0 
+    output = torch.sigmoid(output)
     preds = torch.round(output)
-    correct = (preds == gold).sum().item()
-    acc = correct/gold.shape[0]
+    total += gold.size(0)
+    correct += (preds == gold).sum().item()
+    acc = correct/(total*NUM_CLASSES)
     return acc
 
 def evaluate(model, loader, criterion):
-    #compare inference time, model size(# of params), classification report, prec/recal
     epoch_loss = 0
     epoch_acc = 0
     with torch.no_grad():
         for batch in loader:
-            pred = model(batch['image'])
-            loss = criterion(pred, batch['label'].float())
-            epoch_loss += loss.item()
-            epoch_acc += accuracy(pred, batch['label'])
+            inputs, labels = batch['image'].to(device), batch['label'].to(device)
+            pred = model(inputs)
+            loss = criterion(pred, labels)
+            epoch_loss += loss.item() 
+            epoch_acc += accuracy(pred,labels)
     return epoch_loss/len(loader), epoch_acc/len(loader)
 
+def testing(model, test_loader, criterion):
+    all_predicted = np.zeros((1, NUM_CLASSES))
+    all_labels = np.zeros((1, NUM_CLASSES))
+    evaluation_loss = 0.0
+    evaluation_corrects = 0
+    evaluation_total = 0
+    model.eval()
+    with torch.no_grad():
+        for batch in test_loader:
+            inputs, labels = batch['image'].to(device), batch['label'].to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            evaluation_loss += loss.item()
+            predicted = np.round(torch.sigmoid(outputs).cpu()).cpu()
+            all_predicted = np.append(all_predicted, predicted, axis=0)
+            all_labels = np.append(all_labels, labels.cpu(), axis=0)
+            evaluation_corrects += (predicted.cpu() == labels.cpu()).sum().item()
+            evaluation_total += labels.size(0)
+        report = classification_report(all_labels[1:], all_predicted[1:])
+        print(report)
+    return evaluation_loss/len(test_loader.dataset), evaluation_corrects/(evaluation_total*NUM_CLASSES)*100
 
 # --- training ---
-#taken from and modified: https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html 
+#modified from: https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html 
 def train_model(model, train_loader, dev_loader, criterion, optimizer, num_epochs=10):
     since = time.time()
     val_acc_history = []
@@ -114,27 +165,24 @@ def train_model(model, train_loader, dev_loader, criterion, optimizer, num_epoch
 
         epoch_loss = 0.0
         epoch_acc = 0.0
-        test_epoch_acc = 0.0 #testing
-        # Each epoch has a training and validation phase
-            # Iterate over data.
+        
+        # Iterate over data.
         for batch in train_loader:
-            inputs, labels = batch['image'], batch['label']
+            inputs, labels = batch['image'].to(device), batch['label'].to(device)
 
             # zero the parameter gradients
             optimizer.zero_grad()
             # forward
             outputs = model(inputs)
             loss = criterion(outputs, labels)
-            train_correct = accuracy(outputs, labels)
+
             #backprop
             loss.backward()
             optimizer.step()
 
             # statistics
-            epoch_loss += loss.item()# * inputs.size(0) #is this last part needed? 
+            epoch_loss += loss.item()
             epoch_acc += accuracy(outputs, labels)
-            test_epoch_acc += train_correct #testing
-            #print(epoch_loss, epoch_acc)
 
         print('Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss/len(train_loader), epoch_acc/len(train_loader)))
         dev_loss, dev_acc = evaluate(model, dev_loader, criterion)
@@ -143,20 +191,31 @@ def train_model(model, train_loader, dev_loader, criterion, optimizer, num_epoch
         print('Val Loss: {:.4f} Val Acc: {:.4f}'.format(dev_loss, dev_acc))
         
         # copy the model
-        if epoch_acc > best_acc:
-            best_acc = epoch_acc
+        if (epoch_acc/len(train_loader)) > best_acc:
+            best_acc = epoch_acc/len(train_loader)
             best_model_wts = copy.deepcopy(model.state_dict())
             
         #TODO: add early stopping
-        print()
 
     time_elapsed = time.time() - since
     print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
+    print('Best Val Acc: {:4f}'.format(best_acc))
 
-    # load best model weights
+    #load best model weights
     model.load_state_dict(best_model_wts)
     return model, val_acc_history
 
 # --- run training --- 
+#initialize, feature_extract = True means only final layer is updated while False means full finetune 
+model_ft, input_size = initialize_model("densenet", NUM_CLASSES, feature_extract=True, use_pretrained=True) 
+
+#also from pytorch: https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html 
+params_to_update = model_ft.parameters()
+print("Params to learn:")
+params_to_update = []
+for name,param in model_ft.named_parameters():
+    if param.requires_grad == True:
+        params_to_update.append(param)
+optimizer_ft = optim.SGD(params_to_update, lr=0.001)
+
 model_ft, hist = train_model(model_ft, train_loader, dev_loader, criterion, optimizer_ft, num_epochs=10)
